401296ba8fbb03ee3e3fd66dcba7c2dcede7c3d9
yuuki3-ll <yoshimi.yuki@jaea.go.jp>
2025-04-24 10:39:48 +0900

    Update Plot_PolyFIT_250401.C
    Added a process to create a nice directory structure.
    ramen

--- Plot_PolyFIT_250401.C
+++ Plot_PolyFIT_250401.C
@@ -179,12 +179,20 @@ void Plot_PolyFIT_250401(char *file, Double_t Ql_MIN, Double_t Ql_MAX)
 		cout << "**********************************************************************" << endl << endl;
 	}
 
+	fs::path dir = Form("%s%s",MainDir.Data(), RootDir.Data());
+        if (!fs::exists(dir)) {
+                fs::create_directory(dir);
+                std::cout << "root dir mkdir now!" << dir << std::endl;
+        } else {
+                std::cout << "rootdir already mkdir" << dir << std::endl;
+        }
+
 	fs::path dir = Form("%s%s/Th%04.0f%s",MainDir.Data(), RootDir.Data(), Ql_MIN, RootPlotDir.Data());
         if (!fs::exists(dir)) {
                 fs::create_directory(dir);
-                std::cout << "mkdir now!" << dir << std::endl;
+                std::cout << "Thxx dir mkdir now!" << dir << std::endl;
         } else {
-                std::cout << "already mkdir" << dir << std::endl;
+                std::cout << "Thxx dir already mkdir" << dir << std::endl;
         }
 
 
